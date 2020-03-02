88ec077e240803f117bbc6bb5cad5399ebd343d2
YeungKC <I@YeungKC.com>
2020-03-02 23:39:06 +0800

    refactor: change to js

---
+++ index.js
@@ -0,0 +1,23 @@
+var replaceRegex = /\D/g
+
+function autoReplier() {
+  var labelObj = GmailApp.getUserLabelByName('SMS')
+  var unreadCount = labelObj.getUnreadCount()
+  var threads = labelObj.getThreads()
+  var thread
+
+  for (var i = 0; i < unreadCount; i++) {
+    thread = threads[i]
+    if (thread.isUnread()) {
+      thread.markRead()
+
+      var phone = thread.getMessages()[0].getSubject().replace(replaceRegex, '')
+      var contacts = ContactsApp.getContactsByPhone(phone)
+
+      console.log('phone: ' + phone + ', contacts.length: ' + contacts.length)
+      if (contacts.length) continue
+
+      thread.reply("Hey, I received your message and may reply to you later.")
+    }
+  }
+}
